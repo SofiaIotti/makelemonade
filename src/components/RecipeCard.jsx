a5dc0a9be8c7fb2739c205ae7293c9f8b066b44e
SofiaIotti <137886054+SofiaIotti@users.noreply.github.com>
2024-09-26 09:44:07 +0200

    Update RecipeCard.jsx

--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Link} from 'react-router-dom'
+import defaultRecipeImage from '../assets/default-recipe-image.png';
 
 const RecipeCard = ({recipe}) => {
     return (
@@ -8,7 +9,7 @@ const RecipeCard = ({recipe}) => {
                 <img src={recipe.image} alt={recipe.title}
                     onError={(e) => {
                         e.target.onerror = null;
-                        e.target.src = "/assets/default-recipe-image.png";
+                        e.target.src = defaultRecipeImage;
                     }}
                 ></img>
                 <h3>{recipe.title}</h3>
